be44247cf0edf2bdad6e81f918c267880dcaafe5
Gurdit Rehal <gurdit_rehal@yahoo.co.uk>
2024-07-20 00:45:45 +0100

    Stylised board and added Tile component

--- src/components/Minefield.js
+++ src/components/Minefield.js
@@ -1,38 +1,13 @@
-import { TILE_TYPE, TILE_STATE } from "../helpers/values";
-
-function displayTile(tile){
-    if (tile.state === TILE_STATE.Hidden){
-        return "H";
-    }
-    else if (tile.state === TILE_STATE.Flagged){
-        return "F";
-    }
-    else if (tile.state === TILE_STATE.Question){
-        return "?";
-    }
-    else if (tile.state === TILE_STATE.Revealed){
-        if (tile.type === TILE_TYPE.Safe){
-            //TODO: Compute number of adjacent tiles
-            return " ";
-        }
-        return "M";
-    }
-}
+import Tile from "./Tile.js"
 
 export default function Minefield({ minefield, onSelectTile }){
     return (
-        <ol>
+        <ol id="game-board">
             {minefield.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((tile, columnIndex) => (
-                            <li key={columnIndex}>
-                                <button
-                                    onClick={() => onSelectTile(rowIndex, columnIndex)}
-                                >
-                                    {displayTile(tile)}
-                                </button>
-                            </li>
+                            <Tile tile={tile} rowIndex={rowIndex} columnIndex={columnIndex} onSelectTile={onSelectTile} minefield={minefield}/>
                         ))}
                     </ol>
                 </li>
